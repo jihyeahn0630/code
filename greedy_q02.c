5d47fe9bc47c51858b1f2fa57c0b66801c00c9e2
jihyeahn0630 <85170687+jihyeahn0630@users.noreply.github.com>
2021-06-05 17:44:50 +0900

    Create greedy_q02.c

---
+++ greedy_q02.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char input[22];
+
+int main() {
+  int i;
+	int val;
+  int total = 0;
+	
+	scanf("%s", input);
+  
+	for(i=0; input[i]; i++){
+		
+		val = input[i]-'0';
+		
+		if(val == 0){
+			total = total + val;
+		}else{
+			if(total==0)
+			  total = total + val;
+			else
+				total = total * val;
+		}
+			
+	}
+		
+	
+	printf("%d ",total);
+  
+	return 0;
+}

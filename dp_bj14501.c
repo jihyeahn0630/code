e1fbaf5f33a52800b95cba5405366a404338871a
jihyeahn0630 <85170687+jihyeahn0630@users.noreply.github.com>
2021-06-12 14:22:20 +0900

    Update dp_bj14501.c

--- dp_bj14501.c
+++ dp_bj14501.c
@@ -33,12 +33,12 @@ int main() {
         
 	
 	  for(i=N-1; i>=0; i--){
-			t = i + time[i];
+		t = i + time[i];
 	   if(t > N){
 		dp[i] = max;
 		}
 		else{	
-			
+	         //직전까지의 최대값(max)과 현재의 pay를 택했을 경우 현재의 상담이 종료됐을 시점의 최대값과의 합을 비교
 		 dp[i]= getMax(pay[i]+dp[t],max);
 		 max = dp[i];
 

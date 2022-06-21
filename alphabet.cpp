beeac4ee0bab80b0a7151ca5e1e224d489a68bef
jihyeahn0630 <85170687+jihyeahn0630@users.noreply.github.com>
2022-06-22 01:03:13 +0900

    Update alphabet.cpp

--- alphabet.cpp
+++ alphabet.cpp
@@ -1,73 +1,44 @@
-#include<set>
-#include<map>
-#include<cassert>
-#include<vector>
-#include<cstdio>
-#include<cstring>
-#include<memory>
-#include<algorithm>
-#include<string>
-#include<queue>
-#include<iostream>
-
+#include <iostream>
+#include <vector>
+#include <string>
 using namespace std;
-vector<string> v(30);
-vector<char> abc;
-bool visit[30];
-int dy[]={0,0,-1,1};
-int dx[]={-1,1,0,0};
-int r,c;
-int maxS=-1;
 
-bool check(vector<char>& a, char val){ 
-	int nsize = a.size();
-	
-	if(nsize > maxS)
-		maxS = nsize;
-	
-	for(int i=0;i<nsize;i++){
-		if(a[i]==val)
-			return false;
-	}
-	return true;
+int dx[] = {0, 0, 1, -1};
+int dy[] = {1, -1, 0, 0};
+int n, m;
+int go(vector<string> &board, vector<bool> &check, int x, int y) {
+    int ans = 0;
+    for (int k=0; k<4; k++) {
+         int nx = x+dx[k];
+         int ny = y+dy[k];
+ 
+		
+        if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
+            if (check[board[nx][ny]-'A'] == false) {
+             check[board[nx][ny]-'A'] = true;
+             int next = go(board, check, nx, ny);
+             if (ans < next) {
+                 ans = next;
+                 }
+             check[board[nx][ny]-'A'] = false;
+             }
+         }
+     }
+     return ans + 1;
 }
-
-
-void solve(int y, int x){
-		
-	
-	for(int i=0 ; i < 4; i++){
-		int ny = y+dy[i];
-		int nx = x+dx[i];
-		
-		if(ny<0 || ny>=r || nx<0 || ny>=c)
-			continue;
-		
-		if(check(abc, v[ny][nx])){
-			abc.push_back(v[ny][nx]);
-			solve(ny, nx);
-			abc.pop_back();
-		}	
-	}
-	
-}
-
-
 
 int main() {
+   
+     cin >> n >> m;
+     vector<string> board(n);
+    
+     for (int i=0; i<n; i++) {
+         cin >> board[i];
+     }
+    
+     vector<bool> check(26);
+     check[board[0][0]-'A'] = true;
+     cout << go(board, check, 0, 0) << endl;
  
-	cin >> r >> c ;
-	
-	for(int i=0; i<r; i++){
-		for(int j=0; j<c; j++){
-			cin >> v[i][j];
-		}
-  }
-
-	abc.push_back(v[0][0]);
-	solve(0,0);
-	cout << maxS <<endl;
-	
-	
-	return 0;
+    return 0;
 }
